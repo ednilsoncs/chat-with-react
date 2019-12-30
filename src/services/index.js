5f2647113679fce9f50d46c10d653d0867135e0d
ednilsonCS <ednilsoncs@dcomp.ufs.br>
2019-12-30 00:22:45 -0300

    feat: added websocket

---
+++ src/services/index.js
@@ -0,0 +1,3 @@
+import ws from './websocket';
+
+export { ws };
---
+++ src/services/websocket.js
@@ -0,0 +1,5 @@
+import Ws from '@adonisjs/websocket-client';
+
+const ws = Ws('ws://adonischat.herokuapp.com');
+
+export default ws;

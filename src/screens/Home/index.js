5fbc45d03368a2c7161f84db2380c330d263cbea
ednilsonCS <ednilsoncs@dcomp.ufs.br>
2019-12-30 00:23:16 -0300

    feat: added initial chat

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ws } from '../../services';
 import logo from '../../assets/images/logo.svg';
-import './styles.css';
 
+ws.connect();
+ws.on('open', () => {
+  // setConnect(true);
+});
+const chat = ws.subscribe('chat');
 function App() {
+  const [isConnect, setConnect] = useState(false);
+  const [message, setMessage] = useState('');
+  const [listMessage, setListMensage] = useState([]);
+  useEffect(() => {
+    loadingMensagem();
+  }, []);
+  const loadingMensagem = async () => {
+    await chat.on('message', (data) => {
+      const list = listMessage;
+      list.push(data.text);
+    });
+  };
+  const sendMensage = (e) => {
+    e.preventDefault();
+    chat.emit('message', { text: message });
+    loadingMensagem();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="login-screen" className="hero column is-fullheight">
+      <div className="container">
+        <div>
+
+          {listMessage.map((item) => <><text>{item}</text><br /></>)}
+        </div>
+        <form onSubmit={(e) => { sendMensage(e); setMessage(''); }}>
+          <textarea
+            id="ta"
+            name="textarea"
+            value={message}
+            onChange={(e) => (setMessage(e.target.value))}
+          />
+          <button type="submit">SEND</button>
+        </form>
+      </div>
     </div>
   );
 }
